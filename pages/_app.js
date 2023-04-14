ba6c651a40b3d5cdabda558b8cff046f0255421c
Alucard2169 <ashish10deepanshu@gmail.com>
2023-04-14 14:12:58 +0530

    updated index.js code with custom components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
+import { Roboto_Flex } from "@next/font/google";
+
+const roboto_flex = Roboto_Flex({
+  subsets: ["latin"],
+  weight: ["300", "500", "700", "800"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout className={roboto_flex.className}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
