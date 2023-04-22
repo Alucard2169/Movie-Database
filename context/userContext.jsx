123711ec3428de0e99bb8b64037fddbe5417467c
Alucard2169 <ashish10deepanshu@gmail.com>
2023-04-22 18:45:19 +0530

    implemented initial cookie (token) check

--- context/userContext.jsx
+++ context/userContext.jsx
@@ -1,9 +1,26 @@
-const { createContext, useState } = require("react");
+const { createContext, useState, useEffect } = require("react");
 
 export const userContext = createContext(null);
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const initial = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/initial");
+        const data = await response.json();
+
+        if (data.user) {
+          setUser(data.user);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    initial();
+  }, []);
 
   return (
     <userContext.Provider value={{ user, setUser }}>
---
+++ pages/api/initial.js
@@ -0,0 +1,25 @@
+import { User } from "@/models/user";
+import { getCookie } from "cookies-next";
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+  try {
+    const token = getCookie("token", { req, res });
+    if (token) {
+      const data = jwt.verify(token, process.env.JWT_SECRET);
+      const { userId } = data;
+
+      const userData = await User.findById(userId);
+
+      if (!userData) {
+        throw new Error("User doesn't exist in database, try Signing up");
+      }
+      const user = { username: userData.username, email: userData.email };
+      res.status(200).json({ user });
+    } else {
+      throw new Error("Unauthorized action");
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
