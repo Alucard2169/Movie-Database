f486039099498c63fb14bfab4e13deba0bfcc99a
Alucard2169 <ashish10deepanshu@gmail.com>
2023-06-01 13:09:20 +0530

    updated auth design

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: "1mb",
-    },
-  },
+
   output: "standalone",
 };
 
