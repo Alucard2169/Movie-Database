9bc4ce2b54b9dcf07d5ad1b6e35f1ce7150c7f80
Alucard2169 <ashish10deepanshu@gmail.com>
2023-06-11 13:11:31 +0530

    added top-rated movie list

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,23 @@ import Banner from "@/components/Banner";
 import MovieSection from "@/components/MovieSection";
 import homeStyle from "../styles/Home.module.css";
 
-export default function Home({ images, result }) {
+export default function Home({ images, result, topRatedMoviesResult }) {
   return (
     <div className={homeStyle.home}>
       <Banner />
       {/* send movies result as a prop to the MovieSection component */}
-      <h2 className={homeStyle.heading}>Popular Now In Movies</h2>
-      <MovieSection data={{ images, result }} showDeleteButton={false} />
+      <section>
+        <h2 className={homeStyle.heading}>Popular Now In Movies</h2>
+        <MovieSection data={{ images, result }} showDeleteButton={false} />
+      </section>
+      <hr />
+      <section>
+        <h2 className={homeStyle.heading}>Top Rated In Movies</h2>
+        <MovieSection
+          data={{ images, result: topRatedMoviesResult }}
+          showDeleteButton={false}
+        />
+      </section>
     </div>
   );
 }
@@ -22,7 +32,7 @@ export const getServerSideProps = async () => {
   const images = imageData.images;
 
   const movieResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=1`
   );
   const movieData = await movieResponse.json();
 
@@ -32,7 +42,15 @@ export const getServerSideProps = async () => {
   // store the remaining array in result variable
   const result = movieData.results;
 
+  // top rated movies
+  const topRatedMovies = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=1`
+  );
+  const topRatedMoviesData = await topRatedMovies.json();
+  topRatedMoviesData.results.splice(-4);
+  const topRatedMoviesResult = topRatedMoviesData.results;
+
   return {
-    props: { images, result },
+    props: { images, result, topRatedMoviesResult },
   };
 };
