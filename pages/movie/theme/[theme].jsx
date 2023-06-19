ed992d97e4f9b5aec92da6e0bea4452fee8297f7
Alucard2169 <ashish10deepanshu@gmail.com>
2023-06-19 15:32:30 +0530

    implement infinity scroll on theme page

--- pages/movie/theme/[theme].jsx
+++ pages/movie/theme/[theme].jsx
@@ -1,58 +1,46 @@
+import themeStyle from "@/styles/Theme.module.css";
 import { useEffect, useRef, useState } from "react";
 import MovieSection from "@/components/MovieSection";
 import nameStyle from "../../../styles/Name.module.css";
 
-const MovieByName = ({ images, initialResult, error }) => {
+const MovieByName = ({ images, initialResult, error, theme }) => {
   const [result, setResult] = useState(initialResult);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(2);
   const [loading, setLoading] = useState(false);
-  const observer = useRef(null);
   const lastMovieRef = useRef(null);
 
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-
-    const handleObserver = (entries) => {
-      const target = entries[0];
-      if (target.isIntersecting && !loading) {
-        loadMoreData();
-      }
-    };
-
-    observer.current = new IntersectionObserver(handleObserver, options);
-
-    if (lastMovieRef.current) {
-      observer.current.observe(lastMovieRef.current);
-    }
-
-    return () => {
-      if (observer.current) {
-        observer.current.disconnect();
-      }
-    };
-  }, [loading]);
-
-  const loadMoreData = async () => {
+  const fetchMovies = async () => {
     try {
-      setLoading(true);
-      const nextPage = page + 1;
       const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${theme}?api_key=${process.env.API_KEY}&page=${nextPage}`
+        `https://api.themoviedb.org/3/movie/${theme}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`
       );
-      const newData = await response.json();
-      const newResult = newData.results;
-      setResult((prevResult) => [...prevResult, ...newResult]);
-      setPage(nextPage);
+      const data = await response.json();
+      const result = data.results;
+      setResult((prevResult) => [...prevResult, ...result]);
       setLoading(false);
-    } catch (error) {
-      console.error("Error loading more data:", error);
-      setLoading(false);
+    } catch (err) {
+      console.log(err);
     }
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + Math.ceil(window.scrollY) >=
+        document.body.offsetHeight
+      ) {
+        setLoading(true);
+        setPage((prevPage) => prevPage + 1);
+        fetchMovies();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loading]);
 
   if (error) {
     return (
@@ -65,7 +53,20 @@ const MovieByName = ({ images, initialResult, error }) => {
   return (
     <div className={nameStyle.page}>
       <MovieSection data={{ images, result }} showDeleteButton={false} />
-      {loading && <p>Loading more movies...</p>}
+      {loading && (
+        <div className={themeStyle.center}>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+          <div className={themeStyle.wave}></div>
+        </div>
+      )}
       <div ref={lastMovieRef} />
     </div>
   );
@@ -76,21 +77,28 @@ export default MovieByName;
 export const getServerSideProps = async (context) => {
   const { theme } = context.query;
 
-  const imageResponse = await fetch(
-    `https://api.themoviedb.org/3/configuration?api_key=${process.env.API_KEY}`
-  );
-  const imageData = await imageResponse.json();
+  try {
+    const imageResponse = await fetch(
+      `https://api.themoviedb.org/3/configuration?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+    const imageData = await imageResponse.json();
 
-  const images = imageData.images;
+    const images = imageData.images;
 
-  const movieResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/${theme}?api_key=${process.env.API_KEY}&page=1`
-  );
-  const movieData = await movieResponse.json();
+    const movieResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${theme}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=1`
+    );
+    const movieData = await movieResponse.json();
 
-  const initialResult = movieData.results;
+    const initialResult = movieData.results;
 
-  return {
-    props: { images, initialResult },
-  };
+    return {
+      props: { images, initialResult, theme },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: { error: "Failed to fetch data" },
+    };
+  }
 };
